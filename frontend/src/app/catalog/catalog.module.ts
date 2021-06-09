025c88c9c77e94d7c884a7063d14231439e0127a
Bob Jacobs <bob.jacobs@outlook.com>
2021-06-09 17:12:17 -0400

    adding mock data

--- frontend/src/app/catalog/catalog.module.ts
+++ frontend/src/app/catalog/catalog.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { reducer } from './catalog.reducer';
+import { reducer } from './store/catalog.reducer';
 import { CatalogListComponentComponent } from './catalog-list-component/catalog-list-component.component';
 
 
---
+++ frontend/src/app/catalog/model/mock-comics.model.ts
@@ -0,0 +1,11 @@
+import { Comic} from './comic.model';
+import { ComicValue } from './comic-value.model';
+
+export const MOCKCOMICS: Comic[] = [
+  {
+    issueNumber: "A1",
+    title: "Hunt for Treasure",
+    author: "Jeff Aiken",
+    series: "Adventures of Narco"
+  }
+];
