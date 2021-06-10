8e07b9393203a8271b23937214690c6752f2ad6c
Bob Jacobs <bob.jacobs@outlook.com>
2021-06-09 21:12:34 -0400

    data update

--- frontend/src/app/catalog/model/mock-comics.model.ts
+++ frontend/src/app/catalog/model/mock-comics.model.ts
@@ -7,5 +7,29 @@ export const MOCKCOMICS: Comic[] = [
     title: "Hunt for Treasure",
     author: "Jeff Aiken",
     series: "Adventures of Narco"
-  }
+  },
+  {
+    issueNumber: "A2",
+    title: "Narco vs. Dread",
+    author: "Jeff Aiken",
+    series: "Adventures of Narco"
+  },
+  {
+    issueNumber: "A3",
+    title: "Narco Walks Through Fire",
+    author: "Jeff Aiken",
+    series: "Adventures of Narco"
+  },
+  {
+    issueNumber: "A4",
+    title: "Narco & Leonard",
+    author: "Jeff Aiken",
+    series: "Adventures of Narco"
+  },
+  {
+    issueNumber: "B1",
+    title: "Class Is In Session",
+    author: "Nancy Brew",
+    series: "Dr IQ"
+  },
 ];
