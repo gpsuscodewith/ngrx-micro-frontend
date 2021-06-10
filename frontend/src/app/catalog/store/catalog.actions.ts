1f051662800d6c37c0654071d0ce90109313eb9f
Bob Jacobs <bob.jacobs@outlook.com>
2021-06-10 13:27:35 -0400

    deleting commented out code

--- frontend/src/app/catalog/store/catalog.actions.ts
+++ frontend/src/app/catalog/store/catalog.actions.ts
@@ -1,23 +1,6 @@
 import { Action, createAction, props } from '@ngrx/store';
 import { Comic } from '../model/comic.model';
-/*
-export enum CatalogActionTypes {
-  GetComics = '[catalog] Get Comics',
-  SetComics = '[catalog] Set Comics'
-}
 
-export class GetComics implements Action {
-  readonly type = CatalogActionTypes.GetComics;
-  constructor() {}
-}
-
-export class SetComics implements Action {
-  readonly type = CatalogActionTypes.SetComics;
-  constructor(public payload: any) {}
-}
-
-export type CatalogActions = GetComics | SetComics;
-*/
 export const getComics = createAction(
   '[catalog] Get Comics'
 );
